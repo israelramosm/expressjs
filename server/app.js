9174c285eb7d3014be621fd6c6ae286505b3fa8e
Israel Ramos <arnulfo_israel@hotmail.com>
2018-11-30 14:14:29 -0600

    db connection open

--- server/app.js
+++ server/app.js
@@ -1,17 +1,44 @@
 import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
+import session from "express-session";
+import mongo from "connect-mongo";
+import { connect } from "mongoose";
 
-import "./util/secrets"; // Need it to load process.env
+import { MONGODB_URI, SESSION_SECRET } from "./util/secrets"; // Need it to load process.env
 import { logger, errLogger } from "./util/logger";
 
+const MongoStore = mongo(session);
 const app = express();
+
+connect(
+  MONGODB_URI,
+  { useNewUrlParser: true }
+)
+  .then(() => {
+    console.log("** DB connection open");
+  })
+  .catch(error => {
+    console.log(`** Connection error: ${error}`);
+  });
 
 // Express configuration
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    store: new MongoStore({
+      url: MONGODB_URI,
+      autoReconnect: true
+    })
+  })
+);
 
 // Routes
 app.use(logger());
